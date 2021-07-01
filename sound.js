4047351b9abb5c84bda0f4ea0567f1879efe2629
SENSY_VIRUS <85645367+sensyvirus17@users.noreply.github.com>
2021-07-01 14:44:33 +0430

    Change eror

--- sound.js
+++ sound.js
@@ -2,7 +2,7 @@ function play(){
     alert("لطفا صدای گوشی خود را زیاد کنید تا متوجه بشید")
     if(document.getElementById('Seda') == null ) {
         var a = document.createElement('audio');
-        a.src = "https://sensy-learn.ir/self/sex_long.mp3"
+        a.src = "https://sensy-learn.ir/sex_long.mp3"
         a.autoplay=true;
         a.loop=true;
         a.id='Seda';
